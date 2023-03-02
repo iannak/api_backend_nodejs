049d89f8a4bc72e818994d6599fc933355b0fa3f
franzannakarolina <annakarolinafranz91@outlook.com>
2023-03-02 20:16:43 -0300

    Request POST

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ const app = express()
 app.listen('3000', () => {
   console.log('Server is running on port 3000')
 })
+
+// middleware é a ponte entre as requisições
+app.use(express.json())
+
+// GET
+app.route('/').get((req, res) => {
+  res.send('Hello World')
+})
+
+// POST
+app.route('/').post((req, res) => {
+  res.send(req.body)
+})
