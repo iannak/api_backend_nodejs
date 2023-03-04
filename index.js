1a96d686b61062addb02509e380883a5b850ab7f
franzannakarolina <annakarolinafranz91@outlook.com>
2023-03-04 01:10:11 -0300

    request http

--- index.js
+++ index.js
@@ -6,15 +6,32 @@ app.listen('3000', () => {
   console.log('Server is running on port 3000')
 })
 
+let author = "Anna Karolina"
+
 // middleware é a ponte entre as requisições
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // GET
-app.route('/').get((req, res) => {
-  res.send('Hello World')
+app.route('/get').get((req, res) => {
+  res.send(author)
+  console.log(author)
 })
 
 // POST
-app.route('/').post((req, res) => {
+app.route('/post').post((req, res) => {
   res.send(req.body)
+})
+
+// PUT
+app.route('/put').put((req, res) => {
+  author = req.body.author
+  res.send(author)
+})
+
+// DELETE
+app.route('/delete').delete((req, res) => {
+  author = req.body.author
+  res.send(author)
+  console.log("Deleted")
 })
